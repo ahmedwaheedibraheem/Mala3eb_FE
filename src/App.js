4051b6e4c0f5333b44f8b78bd53f2ade282a0be6
Heba Hamed <hebahamed300@gmail.com>
2019-06-17 11:54:18 +0200

    edited again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from "react-router-dom";
 // import PitchLayout from './Containers/Layout/pitch-Layout';
-import Routes from '../src/Routes/routes';
-<<<<<<< HEAD
-import TabsEntity from './Containers/List-Entities/listEntities';
-=======
 import * as classes from './App.module.css';
->>>>>>> 232faf156e98414477a765027693bf026e895e13
+import Routes from '../src/Routes/routes';
+import TabsEntity from './Containers/List-Entities/listEntities';
 
 class App extends Component {
   render() {
