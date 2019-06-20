30405350c144964f2e777466d0d1b30a47aa1e5b
Mohamed_Ibraheem <mohamedibraheem91@yahoo.com>
2019-06-20 16:34:11 +0200

    added routing when create the pitch to the recently created pitch , after the login checks the created player or pitch and routes the user to the apprpriate page

--- src/Containers/Login/login.js
+++ src/Containers/Login/login.js
@@ -61,8 +61,20 @@ class Login extends Component {
             localStorage.setItem('token', response.token);
             // Setting user app
             this.props.setAppUser(response.user);
+
+            if (response.user.playerId && response.user.pitchId.length === 0) {
+                console.log("there is player and no pitches");
+                this.props.history.push(`/profile/${response.user.playerId}`);
+            }
+            else if (response.user.pitchId.length > 0) {
+                console.log("there are pitches to that user");
+                console.log("go to waheeds page");
+                /// route to the entites page
+            }
+            else {
+                this.props.history.push(`/creation`);
+            }
             // There shall be redirect here later ...
-            this.props.history.push(`/profile/${response.user.playerId}`);
         } catch (error) {
             console.log(error)
             // there shall be real error handling here later ...
