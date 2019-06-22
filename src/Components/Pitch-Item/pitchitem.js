1901454fb4d9725fb32b8c6760cdb13f1da7fccf
Mohamed_Ibraheem <mohamedibraheem91@yahoo.com>
2019-06-22 10:33:21 +0200

    added listing for the pitches in the system

--- src/Components/Pitch-Item/pitchitem.js
+++ src/Components/Pitch-Item/pitchitem.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import './pitchitem.module.css';
+import * as classes from './pitchitem.module.css';
 
 const PitchItem = (props) => {
     return (
         <>
-            <div className="card border-black" style={{ marginTop: '2.3rem' }}>
-                <div>
-                </div>
-                <div className="card-body" style={{ paddingTop: '2rem' }}>
+            <div className="card border-black" style={{ marginTop: '2.3rem'}}>
+                <div className={classes.cardbody}>
                     <img src={props.image}></img>
-                    <hr/>
+                    <hr />
                     <span>{props.name}</span>
-                    <hr/>
+                    <hr />
                     <span>{props.address}</span>
+                    <hr />
                     <button onClick={props.showPitch}>احجز</button>
                 </div>
             </div>
--- src/Containers/Layout/collection_layout.js
+++ src/Containers/Layout/collection_layout.js
@@ -2,8 +2,6 @@ import * as collection from "../../API/collection";
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Navbar from '../../Containers/Navbar/navbar';
-import ProfileData from '../../Components/ProfileData/ProfileData';
-import RateView from '../../Containers/Rate/rate'
 import * as classes from './layout.module.css'
 import Player from "./collectionPlayers/collectionPlayers"
 
@@ -18,11 +16,10 @@ class CollectionLayout extends Component {
 
     async componentDidMount() {
         let collectionId = this.props.match.params.collectionId;
-        let data = await collection.getOneCollection("5d0cec76f8edae32640aa446");
+        let data = await collection.getOneCollection("5d0794bc9ec57f2f143bded9");
         this.props.setCollection(data);
 
-        const x = await collection.getAllPlayersInCollection("5d0cec76f8edae32640aa446")
-        // console.log(x)
+        const x = await collection.getAllPlayersInCollection("5d0794bc9ec57f2f143bded9")
         const arr = [];
         for (const key in x) {
             arr.push({
@@ -32,42 +29,23 @@ class CollectionLayout extends Component {
             })
         }
         this.setState({ players: arr })
-        console.log(arr)
-
-
     }
 
-
-
     deleteCollectionHandler = async (colId) => {
-
         await collection.deleteCollection(colId)
         let _id = localStorage.getItem('playerId')
         this.props.history.push(`/profile/${_id}`)
-
     }
-
-    deletePlayerHandler = async () => {
-
-    }
-
-    // inviteHandler = () => {
-    //     this.props.history.push("/searchresult")
-    // }
 
     getPlayersInCollectionHandler = async (colId) => {
         let playersInCollection = await collection.getAllPlayersInCollection(colId)
         console.log(playersInCollection);
-
     }
-
-
 
     joinHandler = async (collectionId) => {
         await collection.joinCollection(collectionId)
         window.location.reload()
     }
-
 
     cancelJoinHandler = async (collectionId) => {
         await collection.cancelJoin(collectionId)
@@ -78,35 +56,19 @@ class CollectionLayout extends Component {
         window.location.replace(`http://localhost:3000/profile/${id}`)
     }
 
-
     render() {
-
-        // let isJoined = this.state.Joined
-        // console.log(isJoined)
-        // console.log(this.state.Joined)
-
-
         let items = this.state.players.map(player => (
             <React.Fragment key={player.id}>
                 <Player
                     image={player.image}
                     name={player.name}
                     clicked={() => this.showProfileHandler(player.id)}
-                // delete={() => this.deleteHandler(player.id)} 
-
                 />
             </React.Fragment>
         ));
 
-
-
-        console.log(this.props.collection);
-
-
         if (this.props.collection) {
-
             return (
-
                 <div className={classes.bgimg}>
                     <div>
                         <div className="row no-gutters">
@@ -121,18 +83,10 @@ class CollectionLayout extends Component {
                                 <div className={classes.collectionheader}>
                                     <button className={classes.collectionname} >تجمع الكبار</button>
                                     <div className={classes.btngroup}>
-
                                         <button
                                             className="btn btn-success"
                                             onClick={() => this.deleteCollectionHandler("5d0c1e5350fdf23ce43fc334")}
                                         >حذف</button>
-
-
-
-
-
-
-
                                         {this.props.collection.players.includes(localStorage.getItem('playerId'))
                                             ?
                                             <button
@@ -144,30 +98,18 @@ class CollectionLayout extends Component {
                                                 className="btn btn-success"
                                                 onClick={() => this.joinHandler("5d0cec76f8edae32640aa446")}
                                             >انضم</button>}
-
-
-
-
-
-
-
-
                                         <button
                                             className="btn btn-success"
                                             onClick={() => this.inviteHandler()}
                                         >دعوة</button>
-
                                     </div>
                                     <div className={classes.number}>
                                         <div style={{ color: 'red', fontSize: '26px', fontWeight: 'bold' }}>4</div>
                                         <div style={{ fontSize: '9px' }}>متبقى</div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-                        {/* joiner card */}
-
                         <div className="row">
                             <div className="col-lg-12">
                                 <div className="card border-black" style={{ marginTop: '2.3rem' }}>
@@ -181,40 +123,25 @@ class CollectionLayout extends Component {
                                 <div className="card" style={{ "textAlign": "right" }}>
                                     <div className="card-body row no-gutters">
                                         <div className={classes.commentImg}>
-                                            {/* <img src={commentImg}></img> */}
                                         </div>
                                         <div className="col md-9">
                                             <h5 className="card-title"></h5>
                                             <h6 className="card-subtitle mb-2 text-muted">
-                                                {/* <RateView></RateView> */}
-
-                                                {/* listing */}
-
-
                                                 {items}
-
-
-
                                             </h6>
                                             <span>{this.props.commentDate}</span>
                                             <p className="card-text"></p>
                                         </div>
-                                        {/* <div className="commentBtns">
-                                            <button className="btn btn-info" style={{ padding: '10px 20px', marginLeft: '10px' }}>عرض</button>
-                                            <button className="btn btn-success" style={{ padding: '10px 20px', marginLeft: '10px' }}>اضافة</button>
-                                        </div> */}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             );
         }
         else return <h1>page not found</h1>;
     }
-
 }
 
 //mapStateToProps
