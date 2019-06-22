9d21771c9f058ed8f9e018661a2d04abef481d28
Mohamed_Ibraheem <mohamedibraheem91@yahoo.com>
2019-06-22 15:21:25 +0200

    added cover image , edited design pitch item and pitch list

--- src/Containers/Pitches-Listing/pitchlist.js
+++ src/Containers/Pitches-Listing/pitchlist.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PitchItem from '../../Components/Pitch-Item/pitchitem';
 import * as pitchAPI from '../../API/pitch';
 import * as pitchActions from '../../Store/Pitch/pitch-actions';
+import Navbar from '../Navbar/navbar';
+import * as classes from './pitchList.module.css';
 
 
 class PitchList extends Component {
@@ -12,7 +14,7 @@ class PitchList extends Component {
         this.props.setPitches(pitches);
     }
 
-    showPitch(id){
+    showPitch(id) {
         this.props.history.push(`/pitch/${id}`);
     }
 
@@ -20,22 +22,30 @@ class PitchList extends Component {
         if (this.props.pitches) {
             return (
                 <>
-                    <div className="card-header" style={{
-                        backgroundColor: '#000', color: 'white', fontWeight: 'bold',
-                        fontSize: 20
-                    }}>الملاعب
-                    {this.props.pitches.map((pitch) => {
-                            return (
-                                <PitchItem
-                                    key={pitch._id}
-                                    name={pitch.name}
-                                    address={pitch.address}
-                                    image={pitch.imgURL}
-                                    showPitch={()=>{this.showPitch(pitch._id)}}
-                                >
-                                </PitchItem>
-                            )
-                        })}
+                <Navbar></Navbar>
+                <div className={classes.bgimg}>
+                    <div className="row no-gutters">
+                        <div className="card border-dark" style={{ marginTop: '1rem', width: '65%',margin:'1.5rem auto' }}>
+                            <div className="card-header" style={{
+                                backgroundColor: '#000', color: 'white', fontWeight: 'bold',
+                                fontSize: 20, direction: 'rtl', textAlign: 'right'
+                            }}>الملاعب </div>
+                            <div className="card-body">
+                                {this.props.pitches.map((pitch) => {
+                                    return (
+                                        <PitchItem
+                                            key={pitch._id}
+                                            name={pitch.name}
+                                            address={pitch.address}
+                                            image={pitch.imgURL}
+                                            showPitch={() => { this.showPitch(pitch._id) }}
+                                        >
+                                        </PitchItem>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
                     </div>
                 </>
             )
